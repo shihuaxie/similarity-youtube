67d508449f2b8ca458856162c2f3ee716e13624d
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-03-30 14:49:39 +1100

    feat: completed sign in btn

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import {FC} from "react";
+
+interface SignOutButtonProps {}
+
+const SignOutButton: FC<SignOutButtonProps> = ({}) =>{
+    return <div></div>
+}
+
+export default SignOutButton;
